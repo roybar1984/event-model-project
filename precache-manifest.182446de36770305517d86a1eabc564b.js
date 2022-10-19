c2485e7f680783ee0511270475865fa73ea75caa
roy bar <roy.bar1984@gmail.com>
2022-10-19 13:03:24 +0300

    Updates

---
+++ precache-manifest.182446de36770305517d86a1eabc564b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "78ba9791ce9e77ddb9f5",
+    "url": "/event-model-project/static/css/main.3ad1a8e2.chunk.css"
+  },
+  {
+    "revision": "78ba9791ce9e77ddb9f5",
+    "url": "/event-model-project/static/js/main.78ba9791.chunk.js"
+  },
+  {
+    "revision": "61cf7f42ded3fcffc961",
+    "url": "/event-model-project/static/js/1.61cf7f42.chunk.js"
+  },
+  {
+    "revision": "13f8a10211f453b6799a",
+    "url": "/event-model-project/static/js/2.13f8a102.chunk.js"
+  },
+  {
+    "revision": "0e833e3b1fd8b04e867e",
+    "url": "/event-model-project/static/js/runtime~main.0e833e3b.js"
+  },
+  {
+    "revision": "53ddbdc26113fa0599cfd9369858c49c",
+    "url": "/event-model-project/static/media/heart.53ddbdc2.svg"
+  },
+  {
+    "revision": "1f86f6c46bf066316c13a9cba815ccfd",
+    "url": "/event-model-project/static/media/IBMPlexMono-Medium.1f86f6c4.ttf"
+  },
+  {
+    "revision": "1b24ac57b90ddccb7b4c8d58742e73a4",
+    "url": "/event-model-project/static/media/Signika-Regular.1b24ac57.ttf"
+  },
+  {
+    "revision": "497ba1f23aa8d4adebe07876df649cfc",
+    "url": "/event-model-project/index.html"
+  }
+];
